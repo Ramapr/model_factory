ebf0cc2015c161d62fc670b5c686c37157c86b9b
Roman <arcabuceroma79@gmail.com>
2024-04-10 20:03:57 +0300

    style

--- app/io.py
+++ app/io.py
@@ -1,19 +1,16 @@
-from fastapi import  HTTPException
 import os
+
 import boto3
+from fastapi import HTTPException
 from starlette.datastructures import UploadFile
 
 
-
-
 def load_from_s3(data: str, base_path: str, credits):
-    """
-
-    """
+    """ """
     # credits
     # data  = "{bucket}/path/file.csv"
-    BUCKET_NAME = data.split('/')[0]
-    src_file_name = data.split('/')[-1]
+    BUCKET_NAME = data.split("/")[0]
+    src_file_name = data.split("/")[-1]
 
     file_path = data.split(BUCKET_NAME)[1][1:]
     target_path = os.path.join(base_path, src_file_name)
@@ -21,11 +18,12 @@ def load_from_s3(data: str, base_path: str, credits):
     # fix creds to os.environ[""]
     try:
         # connect to minio
-        s3 = boto3.resource('s3',
+        s3 = boto3.resource(
+            "s3",
             endpoint_url=credits["S3"],
             aws_access_key_id=credits["S3_USER"],
             aws_secret_access_key=credits["S3_PASS"],
-                        )
+        )
         s3.Bucket(BUCKET_NAME).download_file(file_path, target_path)
         return target_path
 
@@ -39,7 +37,7 @@ def write_file2fs(path, data):
     """
     data_path = os.path.join(path, data.filename)
     contents = data.file.read()
-    with open(data_path, 'wb') as f:
+    with open(data_path, "wb") as f:
         f.write(contents)
     data.file.close()
     return data_path
@@ -51,7 +49,7 @@ def save_data(data, base_path):
         # load from local s3
     else:
         # binary file
-        if data.filename.split('.')[1] not in ['csv', 'parquet']:
-            raise HTTPException(status_code=400, detail='unknown format for data file')
+        if data.filename.split(".")[1] not in ["csv", "parquet"]:
+            raise HTTPException(status_code=400, detail="unknown format for data file")
 
         return write_file2fs(base_path, data)
--- app/utils.py
+++ app/utils.py
@@ -1,25 +1,28 @@
-import os
-import yaml
-import json
-from dotenv import dotenv_values
-import shutil
 import importlib
 import inspect
+import json
+import os
+import shutil
+
+import yaml
+from dotenv import dotenv_values
 
 
-def get_list_of_configs(path: str = './models') -> dict:
+def get_list_of_configs(path: str = "./models") -> dict:
     output = {}
-    models = list(filter(lambda x: not x.startswith('__') and '.' not in x,  os.listdir(path)))
+    models = list(
+        filter(lambda x: not x.startswith("__") and "." not in x, os.listdir(path))
+    )
 
     for m in models:
-        cfg_path = os.path.join(os.path.join(path, m), 'config.yaml')
-        with open(cfg_path, 'r') as cfg_file:
+        cfg_path = os.path.join(os.path.join(path, m), "config.yaml")
+        with open(cfg_path, "r") as cfg_file:
             output[m] = yaml.safe_load(cfg_file)
 
     return output
 
 
-def check_env_var(path: str = './models') -> bool :
+def check_env_var(path: str = "./models") -> bool:
     if ".env" in os.listdir(path):
         credits = dotenv_values(".env")
         # load credentials from .env file
@@ -39,7 +42,7 @@ def create_dir(root_path: str, dirname: str) -> str:
     dir_path = os.path.join(root_path, dirname)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
-        print(f'INFO : dir \'{dirname}\' created')
+        print(f"INFO : dir '{dirname}' created")
     return dir_path
 
 
@@ -53,7 +56,7 @@ def drop_files_from_dir(folder):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
 
 
 def write_file2fs(path, data):
@@ -63,42 +66,57 @@ def write_file2fs(path, data):
     """
     data_path = os.path.join(path, data.filename)
     contents = data.file.read()
-    with open(data_path, 'wb') as f:
+    with open(data_path, "wb") as f:
         f.write(contents)
     data.file.close()
     return data_path
 
 
-def selection(module_name: str, submodule_name:str) -> object:
-    '''
+def selection(module_name: str, submodule_name: str) -> object:
+    """
     move to model-utils
 
     # step 0 find all model modules
     # step 1 find file with model class . file where class model exists
     # step 2 import that module
     # step 3 find name of class and import it
-    '''
-    modules = list(filter(lambda x: not x.startswith('.') and not x.startswith('__') and not x.endswith('.py'), os.listdir('./models')))
+    """
+    modules = list(
+        filter(
+            lambda x: not x.startswith(".")
+            and not x.startswith("__")
+            and not x.endswith(".py"),
+            os.listdir("./models"),
+        )
+    )
     if module_name not in modules:
-        raise Exception('cannot find specifies module name')
+        raise Exception("cannot find specifies module name")
 
-    class_files = list(filter(lambda x: not x.startswith(submodule_name), os.listdir('./' + module_name)))
+    class_files = list(
+        filter(
+            lambda x: not x.startswith(submodule_name), os.listdir("./" + module_name)
+        )
+    )
     if len(class_files) != 1:
-        raise Exception('cannot find target file')
+        raise Exception("cannot find target file")
 
-    class_file = class_files[0].split('.')[0]
-    module = importlib.import_module('models.' + module_name + '.' + class_file)
+    class_file = class_files[0].split(".")[0]
+    module = importlib.import_module("models." + module_name + "." + class_file)
     # get name of classes
 
-    class_list = [cls_name for cls_name, cls_obj in inspect.getmembers(module) if inspect.isclass(cls_obj)]
+    class_list = [
+        cls_name
+        for cls_name, cls_obj in inspect.getmembers(module)
+        if inspect.isclass(cls_obj)
+    ]
     if len(class_list) != 1:
-        raise Exception('cannot find target class name ')
+        raise Exception("cannot find target class name ")
     return getattr(module, class_list[0])
 
 
 def select_model(module_name: str):
-    return selection(module_name, 'model')
+    return selection(module_name, "model")
 
 
 def select_dset(module_name: str):
-    return selection(module_name, 'data')
+    return selection(module_name, "data")
